aba08c076f11fe4ede0737f1ee70c68ca64ae271
Yong Woon Ngiap <cpt2woy@kul-xred5ci2.comptel.com>
2010-08-17 16:57:59 +0800

    posix multithread

---
+++ t.c
@@ -0,0 +1,38 @@
+#include <pthread.h>
+
+typedef struct {
+  int *ar;
+  long n;
+} subarray;
+
+void * incer(void *arg)
+{
+  long i;
+
+  for (i = 0; i < ((subarray *)arg)->n; i++)
+    ((subarray *)arg)->ar[i]++;
+  puts("incer");
+}
+
+
+int main(void)
+{
+  int        ar[100];
+  pthread_t  th1, th2;
+  subarray   sb1, sb2;
+
+
+  sb1.ar = &ar[0];
+  sb1.n  = 50;
+  (void) pthread_create(&th1, NULL, incer, &sb1);
+
+
+  sb2.ar = &ar[50];
+  sb2.n  = 50;
+  (void) pthread_create(&th2, NULL, incer, &sb2);
+
+
+  (void) pthread_join(th1, NULL);
+  (void) pthread_join(th2, NULL);
+  return 0;
+}
