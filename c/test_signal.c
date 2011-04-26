2c8340242d9d3b793d0a3f0889ac9535c5435099
steve <woongiap@gmail.com>
2011-04-26 21:49:23 +0800

    use \r ang sigaction

--- c/test_signal.c
+++ c/test_signal.c
@@ -1,11 +1,12 @@
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 
 static int run;
 
-void handle_usr1(int sig)
+static void handle_usr1(int sig)
 {
 	printf("I got this signal %d\n", sig);
 	run = 0;
@@ -13,10 +14,19 @@ void handle_usr1(int sig)
 
 int main(int argc, char ** args)
 {
+	struct sigaction sa;
+	int count = 1;  
 	run = 1;
-	sigset(SIGUSR1, handle_usr1);
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = handle_usr1;
+	sigaction(SIGUSR1, &sa, NULL);
+	signal(SIGUSR2, SIG_IGN);
+	// sigset is deprecated
+	//sigset(SIGUSR1, handle_usr1);
+	
 	while (run) {
-		puts("running...");
+		fprintf(stdout, "counting %d...   \r", count++);
+		fflush(stdout);
 	}
 	while (!run) {
 		puts("after signal");
