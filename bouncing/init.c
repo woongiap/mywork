37f2d471a9ee3b5cce075ad77d1b1746437fb367
steve-f14-vm <woongiap@yahoo.com>
2011-05-18 15:50:25 +0800

    add input handler

--- bouncing/init.c
+++ bouncing/init.c
@@ -11,29 +11,31 @@
 #define DEBUG(s) if (_DEBUG_ON) fprintf(stderr, "%s\n", s);
 
 #define SCR_WIDTH 400
-#define SCR_HEIGHT 400
+#define SCR_HEIGHT 800
+
 
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
 void die(char *err);
 void *smalloc(size_t size);
 void debug_surface(const SDL_Surface *sf);
+const char *get_eventname(Uint8 type);
 
 int main(int argc, char *argv[])
 {
 	char *cp, *s = "abcdefghijklmnopqrswxyz";
 	SDL_Surface *screen;
 	/* Code to set a blue pixel at the center of the screen */
-    	int x, y, n;
+    	int x, y, n, quit = 0;
     	Uint32 blue;
+    	SDL_Event event;
 
-	DEBUG("Trying to initialize SDL");
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
         	die(SDL_GetError());
     	}    	
 	DEBUG("SDL initialized");
 	// hook this up at exit
     	atexit(SDL_Quit);
-    	screen = SDL_SetVideoMode(SCR_WIDTH, SCR_HEIGHT, 16, SDL_HWSURFACE|SDL_NOFRAME|SDL_ANYFORMAT);
+    	screen = SDL_SetVideoMode(SCR_WIDTH, SCR_HEIGHT, 16, SDL_HWSURFACE|SDL_RESIZABLE|SDL_ANYFORMAT);
     	if (!screen) die("fail to create framebuffer");
     	debug_surface(screen);
 	
@@ -42,8 +44,8 @@ int main(int argc, char *argv[])
     	*/
     	blue = SDL_MapRGB(screen->format, 0x00, 0x00, 0xff);
 
-    	x = screen->w / 2;
-    	y = screen->h / 2;
+    	x = 0;
+    	y = 0;
 
     	/* Lock the screen for direct access to the pixels */
     	if (SDL_MUSTLOCK(screen)) {
@@ -53,9 +55,8 @@ int main(int argc, char *argv[])
         	}
     	}
 
-	for (n = 0; n < 50; n++) {
-    		putpixel(screen, x + n, y + n, blue);
-    	}
+	for (n = 0; n < 10; n++)
+    		putpixel(screen, x + n, y + n, blue);    	
 
     	if (SDL_MUSTLOCK(screen))
         	SDL_UnlockSurface(screen);
@@ -63,7 +64,29 @@ int main(int argc, char *argv[])
     	/* Update just the part of the display that we've changed */
     	SDL_UpdateRect(screen, 0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-	while(1);
+	/* Poll for events. SDL_PollEvent() returns 0 when there are no  */
+  	/* more events on the event queue, our while loop will exit when */
+  	/* that occurs.
+  	*/
+  	SDL_EnableUNICODE(1);
+  	while (!quit) {
+	  	while(SDL_PollEvent(&event)) {
+			DEBUG(get_eventname(event.type));
+	    		switch(event.type) {
+	      		case SDL_KEYDOWN:
+	      			if (event.key.keysym.sym == SDLK_q)
+	      				quit = 1;
+	      			break;
+	      		case SDL_KEYUP:
+				break;
+	      		case SDL_QUIT:
+				quit = 1;
+				break;
+	      		default:
+				break;
+	    		}
+	  	}
+  	}
 	// test smalloc
 	cp = smalloc(8);
 	cp = s;
@@ -79,7 +102,15 @@ int main(int argc, char *argv[])
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
 {
     	int bpp = surface->format->BytesPerPixel;
-    	/* Here p is the address to the pixel we want to set */
+    	/* Here p is the address to the pixel we want to set 
+    	 *
+    	 * <------ pitch ---------->
+    	 * -------------------------
+    	 * |0 |1 |2 |3 |4 |5 |6 |7 |
+    	 * -------------------------
+    	 * |8 |9 |10|11|12|13|14|15|
+    	 * -------------------------
+    	 */
     	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
 
     	switch(bpp) {
@@ -129,5 +160,43 @@ void debug_surface(const SDL_Surface *sf)
 	if (_DEBUG_ON) {
 		fprintf(stderr, "%d bits-per-pixel mode\n", sf->format->BitsPerPixel);
 		fprintf(stderr, "%d bytes-per-pixel mode\n", sf->format->BytesPerPixel);
+		fprintf(stderr, "%d bytes of surface scanline\n", sf->pitch);
+		fprintf(stderr, "%d width\n", sf->w);
+		fprintf(stderr, "%d height\n", sf->h);
 	}
+}
+
+struct event_map {
+	Uint8 type;
+	// longest SDL_MOUSEBUTTONDOWN
+	char name[25];
+};
+
+const char *get_eventname(Uint8 type)
+{
+	static struct event_map map[] = {
+		{1, "SDL_ACTIVEEVENT"},
+		{2, "SDL_KEYDOWN"},
+		{3, "SDL_KEYUP"},		
+		{4, "SDL_MOUSEMOTION"},
+		{5, "SDL_MOUSEBUTTONDOWN"},
+		{6, "SDL_MOUSEBUTTONUP"},		
+		{7, "SDL_JOYAXISMOTION"},
+		{8, "SDL_JOYBALLMOTION"},
+		{9, "SDL_JOYHATMOTION"},
+		{10, "SDL_JOYBUTTONDOWN"},
+		{11, "SDL_JOYBUTTONUP"},		
+		{12, "SDL_QUIT "},
+		{13, "SDL_SYSWMEVENT"},
+		{14, "SDL_VIDEORESIZE"},
+		{15, "SDL_VIDEOEXPOSE"},
+		{16, "SDL_USEREVENT"},
+		{-1, NULL}
+	};
+	int i;
+	for (i = 0; map[i].type != -1; i++) {
+		if (map[i].type == type)
+			return map[i].name;
+	}
+	return NULL;
 }
