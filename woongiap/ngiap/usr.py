32033ee3275ed3214418eff6feb267d0d6a5b263
steven <woongiap@hotmail.com>
2012-05-14 15:21:37 +0800

    half way, how to send mail?

--- woongiap/ngiap/usr.py
+++ woongiap/ngiap/usr.py
@@ -1,31 +1,20 @@
-<?php
-require_once('mod/db.inc.php');
-require_once('mod/User.class.php');
+UID_MAX = 2147483646
 
-define('UID_MAX', 2147483646);
-function user_spare_id() {
-	return mt_rand(UID_MAX/2+1, UID_MAX);
-}
-function user_new_id() {
-	return mt_rand(123, UID_MAX/2);
-}
-function get_confirm_code() {
-	return mt_rand(1000000001, PHP_INT_MAX);
-}
+def user_spare_id():
+	return mt_rand(UID_MAX/2+1, UID_MAX)
 
-function valid_login_name($s) {
-	$q = "select user_id from k_user where user_name='$s'";
-	$mysqli = k_get_mysqli();
-	if ($result = $mysqli->query($q)) {
-		$num_rows = $result->num_rows;
-		$mysqli->close();
-		return $num_rows == 0;
-	} else {
-		$mysqli->close();
-		return false;
-	}
-}
-function user_login($username, $pw) {	
+def user_new_id():
+	return mt_rand(123, UID_MAX/2)
+
+def get_confirm_code():
+	return mt_rand(1000000001, PHP_INT_MAX)
+
+def valid_login_name(s):
+	q = db.GqlQuery("SELECT __id FROM User " +
+                "WHERE __username = :1", s)
+	return q.get() is None
+
+def user_login(username, pw):
 	$q = "select user_id, display_name from k_user 
 		where user_name='$username' and user_passwd=sha('$pw') and ustate=1";
 	$user;
@@ -41,12 +30,12 @@ function user_login($username, $pw) {
 	}
 	$mysqli->close();
 	return $user; // num of row returned must be exactly 1
-}
 
-define('ACT_SUCCESS', 1);
-define('ACT_NO_USER', 2);
-define('ACT_INVALID_CODE', 3);
-function user_activate($email, $code) {	
+ACT_SUCCESS = 1
+ACT_NO_USER = 2
+ACT_INVALID_CODE = 3
+
+def user_activate(email, code):
 	$return_code = ACT_SUCCESS;;
 	$q = "select last_key from k_user where user_name='$email'";
 	$mysqli = k_get_mysqli();
@@ -71,9 +60,8 @@ function user_activate($email, $code) {
 	}	
 	$mysqli->close();
 	return $return_code;	
-}
 
-function password_change($user_id, $old_password, $new_password) {
+def password_change(user_id, old_password, new_password):
 	$q = "update k_user set user_passwd=sha('$new_password')
 			where user_id=$user_id and user_passwd=sha('$old_password')";
 	$ret_code = 0;
@@ -83,9 +71,8 @@ function password_change($user_id, $old_password, $new_password) {
 	}
 	$mysqli->close();
 	return $ret_code;
-}
 
-function password_reset($email, $code, $newpassword) {
+def password_reset(email, code, newpassword):
 	$q = "update k_user set last_key=NULL, user_passwd=sha('$newpassword')
 			where user_name='$email' and last_key='$code'";
 	$ret_code = 0;
@@ -95,9 +82,8 @@ function password_reset($email, $code, $newpassword) {
 	}
 	$mysqli->close();
 	return $ret_code;
-}
 
-function send_forgotpw_email($email, $code) {
+def send_forgotpw_email(email, code):
 	require_once('mod/mail.inc.php');
 	$recipients[$email] = 'Klasz User';
 	$email = urlencode($email);
@@ -125,8 +111,7 @@ From Klasz Team
 altmsg;
 	send_mail($recipients, "Klasz Login Retrieval", $body, $alt_body);			
 	
-}
-function password_reset_request($email) {
+def password_reset_request(email):
 	$code = get_confirm_code();
 	$q = "update k_user set last_key='$code' where user_name='$email' and last_key is null";
 	$ret_code = 1; // ignore error if email not valid, no email sent anyway
@@ -139,9 +124,8 @@ function password_reset_request($email) {
 	}
 	$mysqli->close();
 	return $ret_code;
-}
 
-function get_user($uid) {
+def get_user(uid):
 	$q = "select * from k_user where user_id=$uid";
 	$mysqli = k_get_mysqli();
 	$user;
@@ -152,16 +136,4 @@ function get_user($uid) {
 		$user->setDisplayname($row['display_name']);
 	}
 	$mysqli->close();
-	return $user;
-}
-
-/*
- * TODO method usage
- */
-function user_update($user_id, $column_name, $column_value) {
-	$q = "update k_user set $column_name='$column_value' where user_id=$user_id";
-	$mysqli = k_get_mysqli();
-	$mysqli->query($q);
-	return true;
-}
-?>
+	return $user
