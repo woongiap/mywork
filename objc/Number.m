d7b2b462b30fe6e1f05e8591b4ebef435c27e694
steveny <steve@fake.com>
2012-08-28 14:31:20 +0800

    add objective-c source file
    it is a very good OO language
    and I agree

---
+++ objc/Number.m
@@ -0,0 +1,35 @@
+// hello.m
+#import <objc/Object.h>
+#import <stdio.h>
+
+@interface Number: Object
+{
+@public
+    int number;
+    
+}
+
+- (void)printNum;
+
+@end
+
+@implementation Number: Object
+
+
+- (void)printNum
+{
+    printf("%d\n", number);
+}
+
+@end
+
+int main(void)
+{
+    Number *myNumber = [Number new]; // equal to [[Number alloc] init]
+
+    myNumber->number = 6;
+
+    [myNumber printNum];
+
+    return 0;
+}
